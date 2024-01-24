f90481c31b306eab67a96b464d9412434d809853
Timo Tran <dai.timo.tran@gmail.com>
2024-01-24 11:43:43 +0100

    Cleaned up file

--- Start.py
+++ Start.py
@@ -52,7 +52,6 @@ def app():
                 session = r.text
                 session = et.fromstring(session)
                 createStreamlitSession(session, email, password)
-                #save_credentials(email, password, credentialPath)
 
                 #check if login was successfull
                 if st.session_state.sessionId == None:
@@ -97,60 +96,6 @@ def createStreamlitSession(auth_session, email, password):
 
     st.session_state.email = email
     st.session_state.password = password
-
-def save_credentials(email, password, credentials_path):
-    """
-    Save the provided email and password to the credentials file.
-
-    Args:
-        email (str): The email to be saved.
-        password (str): The password to be saved.
-        credentials_path (str): The path to the credentials file.
-
-    Returns:
-        None
-    """
-    # Path to the credentials.py file
-    credentials_path = credentials_path + st.session_state.sessionId + '.py'
-
-    # If the file doesn't exist, create it
-    if not Path(credentials_path).is_file():
-        create_credentials_file(credentials_path)
-        return
-
-    # Read the existing content
-    with open(credentials_path, 'r') as file:
-        content = file.readlines()
-
-    # Modify the content
-    for i, line in enumerate(content):
-        if 'login' in line:
-            content[i] = f'login    = "{email}"\n'
-        elif 'password' in line:
-            content[i] = f'password = "{password}"\n'
-
-    # Write the modified content back
-    with open(credentials_path, 'w') as file:
-        file.writelines(content)
-
-def create_credentials_file(file_name, login='', password=''):
-    """
-    Create a credentials file with the provided login and password.
-
-    Args:
-        file_name (str): The name of the file to create.
-        login (str, optional): The login to be included in the credentials file. Defaults to an empty string.
-        password (str, optional): The password to be included in the credentials file. Defaults to an empty string.
-    """
-
-    content = f"""# -*- coding: utf-8 -*-
-    login    = "{login}"
-    password = "{password}"
-    """
-
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(content)
-
 
 if __name__ == "__main__":
     app()
